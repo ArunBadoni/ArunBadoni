8575e46d8bfc8c175af96ea60e0a6c8582673450
ArunBadoni <59121464+ArunBadoni@users.noreply.github.com>
2021-09-22 10:53:47 +0530

    SmallCode
    Testing

---
+++ MyFile.cs
@@ -0,0 +1,16 @@
+class Geeks 
+{
+          
+        // Main Method
+        static void Main(string[] args) 
+        {
+              
+            // statement
+            // printing Hello World!
+            Console.WriteLine("Hello World!");
+              
+            // To prevents the screen from 
+            // running and closing quickly
+            Console.ReadKey();
+        }
+ }
